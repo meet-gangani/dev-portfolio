f8dcb5547e72c08194e394c7a30c193dc03d251c
Meet Gangani <meetgangani71@gmail.com>
2022-12-22 22:43:03 +0530

    - initialize project configs

---
+++ src/components/Home/Type.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Typewriter from 'typewriter-effect'
+
+function Type() {
+  return (
+      <Typewriter
+          options={{
+            strings: [
+              'Javascript Developer',
+              'MERN Stack Developer',
+              'ReactJS Developer',
+              'NodeJS Developer',
+              'NextJS Developer',
+            ],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 70
+          }}
+      />
+  )
+}
+
+export default Type
